2d145791b33382ad7b7e0db75f8789cb5edc1fdc
sunny kumar singh <sunnyharshsunny12345@gmail.com>
2018-04-18 18:18:22 +0530

    Add files via upload
    steps of run program
    1. npm install
    2. bower install
    3. add bootstrap css in index.html page index.html page in public folder
    4. nodemon app.js

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Logo =()=>{
+    return(
+        <div className="logo">
+           <span>Macarthur</span>
+           <span>Automotive</span>
+        </div>
+    )
+}
